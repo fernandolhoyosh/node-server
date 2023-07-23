8eb99da6387748def67488e7088db010660dcaca
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-07-23 16:08:39 -0500

    add funtions delete and complete task

--- index.mjs
+++ index.mjs
@@ -17,33 +17,65 @@ const addTask = () => {
   console.log(chalk.bgGreen.bold("Task successfully added"));
 };
 
+const deleteTask = () => {
+    const idTask = readLine.question("Enter the id of the task to be deleted: ");
+    if(taskList.find((task) => task.id === idTask)){
+        const index = taskList.findIndex((task) => task.id === idTask);
+        const taskName = taskList[index].description;
+        taskList.splice(index,1);
+        console.log(chalk.bgYellow.bold(`task [${taskName}] successfully eliminated`));
+    }else{
+        console.log(chalk.red.bold("ID invalid or does not exist in the list. Try again"));
+    }
+}
+
+const completeTask = () => {
+    const idTask = readLine.question("Enter the id of the task to be completed: ");
+    if (taskList.find((task) => task.id === idTask)) {
+        taskList.forEach((task) => {
+            if (task.id === idTask) {
+                task.status = true;
+                console.log(chalk.green(`Task [${task.description}] completed!`));
+            }
+        });
+    } else {
+        console.log(chalk.red.bold("ID invalid or does not exist in the list. Try again"));
+    }
+}
+
 const printTasks = () => {
-    console.log(chalk.blue.underline("\nTask list:\n"));
+    console.log(chalk.blue.underline.bold("\nTask list:\n"));
     taskList.map((task)=>{
-        console.log(task.description);
+        const id = chalk.cyan.bold(task.id);
+        const description = chalk.cyan.bold(task.description);
+        const status = task.status ? chalk.green("Completed") : chalk.yellow("Pending");
+        console.log(`id: ${id} | task: ${description} | status: ${status}`);
     });
 }
 
 const startApp = () => {
   let exitApp = false;
+
   while (!exitApp) {
-    console.log(chalk.bgCyan.bold("\nTask list App"));
-    console.log(chalk.inverse("Select an option:"));
+    console.log(chalk.bgCyan.bold("\nTask list Node"));
+    console.log(chalk.bold("Select an option:"));
     console.log("1. Add task");
     console.log("2. Delete task");
     console.log("3. Complete task");
     console.log("4. Show tasks");
     console.log("5. Exit");
-    const option = parseInt(readLine.question(chalk.inverse("> ")));
+
+    const option = parseInt(readLine.question(chalk.bold("> ")));
 
     switch (option) {
       case 1:
         addTask();
         break;
       case 2:
-        console.log(chalk.bgYellow.bold("Delete success task"));
+        deleteTask();
         break;
       case 3:
+        completeTask();
         break;
       case 4:
         printTasks();
