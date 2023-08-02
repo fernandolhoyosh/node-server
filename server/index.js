e6d625e87fe4cb7d58c7de63ec675d77051c9aa8
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-08-02 16:54:58 -0500

    Task list server validation routes

--- server/index.js
+++ server/index.js
@@ -6,15 +6,25 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   //Definir rutas
-
   console.log("Se ha realizado una solicitud al servidor.");
 
   // Convertimos la lista de tareas recibida del modulo en formato JSON
   const data = JSON.stringify(moduleServiceTasks());
 
-  // Enviar la respuesta en formato JSON al cliente con status 200 OK y el tipo de contenido
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(data);
+  if (req.url === '/' && req.method === 'GET') {
+    // Mensaje de bienvenida al cliente en la ruta de inicio
+    res.writeHead(200);
+    res.end("Welcome to the server dear customer!");
+  } else if (req.url === '/tasks' && req.method === 'GET') {
+    // Si la ruta es /task el server responde un formato JSON de la lista de tareas al cliente con status 200 OK y el tipo de contenido que es JSON
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
+  } else {
+    // Si la ruta no existe el server responde con un codigo 404
+    res.writeHead(404);
+    res.end("Error 404 Not Found");
+  }
+
 });
 
 // Iniciamos el servidor
