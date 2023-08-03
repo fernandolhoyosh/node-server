6161594c9f394abbb19c397ba1cf53e29bd8d15f
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-08-02 19:25:29 -0500

    Module task list data

--- data/tasks.js
+++ data/tasks.js
@@ -1,24 +1,30 @@
-const tasksList = [
-  {
-    id: 1,
-    description: "Learn React",
-    completed: false,
-  },
-  {
-    id: 2,
-    description: "Buy a new red car",
-    completed: true,
-  },
-  {
-    id: 3,
-    description: "Working out at the gym",
-    completed: true,
-  },
-  {
-    id: 4,
-    description: "Study Express",
-    completed: false,
-  },
-];
+const tasks = () => {
 
-module.exports = { tasksList };
+  const taskList = [
+    {
+      id: 1,
+      description: "Learn React",
+      completed: false,
+    },
+    {
+      id: 2,
+      description: "Buy a new red car",
+      completed: true,
+    },
+    {
+      id: 3,
+      description: "Working out at the gym",
+      completed: true,
+    },
+    {
+      id: 4,
+      description: "Study Express",
+      completed: false,
+    },
+  ];
+
+  return taskList;
+
+};
+
+module.exports = tasks;
